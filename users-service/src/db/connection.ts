3269a9a6ba12d219efc4d10708762800038fa313
poposki317 <philipedekobi@gmail.com>
2022-05-28 15:03:35 +0100

    set up usersessions in db

--- users-service/src/db/connection.ts
+++ users-service/src/db/connection.ts
@@ -2,13 +2,13 @@ import config from "config";
 
 import { Connection, createConnection } from "typeorm";
 
-import { User } from "./entities";
+import { User, UserSession } from "./entities";
 
 let connection: Connection;
 
 export const initConnection = async () => {
     connection = await createConnection({
-        entities: [User],
+        entities: [User, UserSession],
         type: "mysql",
         url: config.get("USERS_SERVICE_DB_URL") as string
     });
